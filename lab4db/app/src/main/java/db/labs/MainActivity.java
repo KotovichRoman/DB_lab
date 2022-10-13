50e1d0a8178a4540b41416d7f84fcf5e69cd60ad
KotovichRoman <koshik.228@yandex.by>
2022-10-13 09:17:31 +0300

    add methods for click button to lab4

--- lab4db/app/src/main/java/db/labs/MainActivity.java
+++ lab4db/app/src/main/java/db/labs/MainActivity.java
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    BufferedWriter bw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +22,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String fname = "Base_Lab.txt";
+        File f = new File(super.getFilesDir(), fname);
         boolean rc = ExistBase(fname);
 
         if (!rc) {
             AlertDialog.Builder b = new AlertDialog.Builder(this);
-            b.setTitle("Создаётся файл " + fname).setPositiveButton("Да",
-                    new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Log.d("Log_02", "Создание файла " + fname);
-                        }
-                    });
-            AlertDialog ad = b.create();
-            ad.show();
+            try {
+                f.createNewFile();
+                Log.d("Log_02", "Файл " + fname + " создан");
+
+                b.setTitle("Создаётся файл " + fname).setPositiveButton("Да",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Log.d("Log_02", "Создание файла " + fname);
+                            }
+                        });
+                AlertDialog ad = b.create();
+                ad.show();
+            }
+            catch (IOException e) {
+                Log.d("Log_02", "Файл " + fname + " не создан");
+            }
         }
+
+        try {
+            FileWriter fw = new FileWriter(f, true);
+            bw = new BufferedWriter(fw);
+        }
+        catch (IOException e) {
+            Log.d("Log_02", "Файл " + fname + " не открыт" + e.getMessage());
+        }
+
+        InputClick();
+    }
+
+    private void InputClick() {
+        EditText name = findViewById(R.id.editTextName);
+        EditText surname = findViewById(R.id.editTextSurname);
+
+        WriteLine(surname.getText().toString(), name.getText().toString());
     }
 
     private boolean ExistBase(String fname) {
@@ -43,5 +74,17 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Log_02", "Файл " + fname + " не найден");
         }
         return rc;
+    }
+
+    private void WriteLine(String surname, String name) {
+        String s = surname + ";" + name + ";" + "\r\n";
+        Log.d("Log_02", s);
+        try {
+            bw.write(s);
+            Log.d("Log_02", "Данные записаны");
+        }
+        catch (IOException e) {
+            Log.d("Log_02", e.getMessage());
+        }
     }
 }
